ae10b8237a1f4152cf7c86c7100cfc556b489ac8
Jeevan Singh <51503835+jeerovan@users.noreply.github.com>
2024-09-09 14:30:08 +0530

    webp size change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,14 @@
   import DarkMode from "$lib/DarkMode.svelte";
   import { onMount } from 'svelte';
 
+  let isMenuOpen = false;
+  function toggleMenu() {
+    isMenuOpen = !isMenuOpen;
+  }
+  function closeMenu() {
+    isMenuOpen = false; // Close the menu
+  }
+
   let position = 0;
   let scrollSpeed = 1.0;
   let images = [
@@ -167,36 +175,50 @@
 
 <!-- Page -->
 <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-  <div class=" flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="/" class="flex items-center space-x-3">
-      <img src="/my-app.webp" class="h-9 rounded-full" alt="Logo">
+  <div class="flex flex-wrap items-center justify-between mx-auto p-4">
+    <!-- Logo -->
+    <a href="/" class="flex items-center space-x-3">
+      <img src="/my-app.webp" height="40px;" width="40px;" class="rounded-full" alt="Logo">
       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MyApp</span>
-  </a>
-  <div class="flex md:order-2 space-x-3 md:space-x-0">
+    </a>
+    
+    <!-- Hamburger Icon -->
+    <div class="flex md:order-2 space-x-3 md:space-x-0">
       <DarkMode />
-      <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+      <button 
+        on:click={toggleMenu} 
+        type="button" 
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
+        aria-controls="navbar-sticky" 
+        aria-expanded={isMenuOpen}
+      >
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
         </svg>
-    </button>
-  </div>
-  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-        <a href="/" class="block py-2 px-3 text-primary-500 hover:text-primary-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
-      </li>
-      <li>
-        <a href="/" class="block py-2 px-3 text-primary-500 hover:text-primary-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-      </li>
-      <li>
-        <a href="/" class="block py-2 px-3 text-primary-500 hover:text-primary-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-      </li>
-      <li>
-        <a href="/" class="block py-2 px-3 text-primary-500 hover:text-primary-600 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-      </li>
-    </ul>
-  </div>
+      </button>
+    </div>
+
+    <!-- Menu items -->
+    <div 
+      class="items-center justify-between w-full md:flex md:w-auto md:order-1 { isMenuOpen ? 'block' : 'hidden' }"
+      id="navbar-sticky"
+    >
+      <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <a href="/" on:click={closeMenu} class="block py-2 px-3 text-primary-700 hover:text-primary-500 dark:text-primary-500 dark:hover:text-primary-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
+        </li>
+        <li>
+          <a href="/" on:click={closeMenu} class="block py-2 px-3 text-primary-700 hover:text-primary-500 dark:text-primary-500 dark:hover:text-primary-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+        </li>
+        <li>
+          <a href="/" on:click={closeMenu} class="block py-2 px-3 text-primary-700 hover:text-primary-500 dark:text-primary-500 dark:hover:text-primary-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+        </li>
+        <li>
+          <a href="/" on:click={closeMenu} class="block py-2 px-3 text-primary-700 hover:text-primary-500 dark:text-primary-500 dark:hover:text-primary-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 dark:hover:bg-gray-700 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
 
